620c4b829db6452bdcb63d2c6c95146032273876
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-03-30 15:18:09 +0500

    second-day

--- src/index.js
+++ src/index.js
@@ -1,81 +1,57 @@
-import { parseInt } from 'lodash';
-import './index.css';
+import Add, { Remove } from './modules/funtions.js';
 
-const todoFullList = document.getElementById('todoFullList');
-const input = document.getElementById('input-type');
-const inputEnter = document.querySelector('.enterSng');
-
-let tasks = [];
-
-inputEnter.addEventListener('click', (() => {
-  if (input.value === '') {
-    alert('please type');
-  } else {
-    const newtask = {
-      description: input.value,
-      completed: false,
-      index: tasks.length,
-    };
-    tasks.push(newtask);
-    todoFullList.innerHTML = '';
-    tasks.forEach((e, index) => {
-      todoFullList.innerHTML += `
-          <div>
-            <li class="todo-child" id=${index}>
-              <div>
-              <input class="checkbox" type="checkbox" ${(e.completed) ? 'checked' : ''}>
-               <span> ${e.description} </span>
-              </div>
-              <div class=bin></div>
-            </li>
-          </div>
-        `;
-    });
-    input.value = '';
-    localStorage.setItem('todo-list', JSON.stringify(tasks));
+const Container = document.getElementById('addtodo');
+const input = document.querySelector('.inp');
+const Tasks = JSON.parse(localStorage.getItem('list')) || [];
+const Display = () => {
+  Tasks.forEach((list, index) => {
+    Container.innerHTML += `<li class="li-list" ><input class="li-list" type="checkbox" data-com="${list.completed}"> <p contenteditable="true" class="paragraph" data-para="${index}">${list.description}</p><i class="fa-regular fa-trash-can" data-index="${index}"></i> <i class="li-list fa-solid fa-ellipsis-vertical"></i></li>`;// eslint-disable-line no-return-assign
+  });
+};
+input.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    Add();
+    window.location.reload();
   }
-}));
-
-window.addEventListener('load', () => {
-  const getItem = JSON.parse(localStorage.getItem('todo-list'));
-  if (getItem) {
-    todoFullList.innerHTML = getItem.map((e, index) => `
-        <div>
-          <li class="todo-child" id=${index}>
-            <div>
-            <input class="checkbox" type="checkbox" ${(e.completed) ? 'checked' : ''}>
-            <span> ${e.description} </span>
-            </div>
-            <div class=bin></div>
-          </li>
-        </div>
-      `).join('');
-    tasks = getItem;
-
-    document.querySelectorAll('.checkbox').forEach((checkbox, index) => {
-      checkbox.addEventListener('click', function () {
-        tasks[index].completed = this.checked;
-        localStorage.setItem('todo-list', JSON.stringify(tasks));
-      });
-    });
+});
+const listItemsContainer = document.querySelector('#addtodo');
+const liLists = document.getElementsByClassName('li-list');
+listItemsContainer.addEventListener('click', (e) => {
+  for (let i = 0; i < liLists.length; i += 1) {
+    liLists[i].classList.remove('active');
+  }
+  if (e.target.classList.contains('li-list')) {
+    e.target.classList.toggle('active');
+  } else if (e.target.classList.contains('paragraph')) {
+    e.target.parentElement.classList.toggle('active');
   }
 });
 
-todoFullList.addEventListener('click', (e) => {
-  if (e.target.classList[0] === 'bin') {
-    e.target.parentElement.remove();
-    const newArray = tasks
-      .filter((element, index) => index !== parseInt(e.target.parentElement.id, 10))
-      .map((element, index) => {
-        element.index = index;
-        return element;
-      });
-    const li = document.querySelectorAll('.todo-child');
-    for (let i = 0; i < newArray.length; i += 1) {
-      newArray[i].index = i;
-      li[i].id = i;
-    }
-    tasks = newArray;
-    localStorage.setItem('todo-list', JSON.stringify(newArray));
+listItemsContainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('fa-trash-can')) {
+    const index = e.target.getAttribute('data-index');
+    Remove(index);
+    window.location.reload();
   }
+});
+const update = () => {
+  for (let i = 0; i < Tasks.length; i += 1) {
+    Tasks[i].index = i + 1;
+  }
+  localStorage.setItem('list', JSON.stringify(Tasks));
+  Display();
+};
+update();
+listItemsContainer.addEventListener('keypress', (e) => {
+  if (e.target.classList.contains('paragraph')) {
+    if (e.key === 'Enter') {
+      if (e.target.textContent === '') {
+        return false;
+      }
+      const num = e.target.getAttribute('data-para');
+      Tasks[num].description = e.target.textContent;
+      window.location.reload();
+    }
+  }
+  return localStorage.setItem('list', JSON.stringify(Tasks));
 });
