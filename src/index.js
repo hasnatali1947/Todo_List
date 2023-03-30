b355d5ab019b2c9e0292e24fa0e0c950f5ec5dc2
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-03-30 12:41:17 +0500

    added add and remove button

--- src/index.js
+++ src/index.js
@@ -1,37 +1,81 @@
+import { parseInt } from 'lodash';
 import './index.css';
 
-const Lists = [
-  {
-    description: 'wash the dishes',
-    completed: true,
-    index: 0,
-  },
-  {
-    description: 'go to masjid',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'Clear all completed',
-    completed: true,
-    index: 2,
-  },
-];
-
 const todoFullList = document.getElementById('todoFullList');
+const input = document.getElementById('input-type');
+const inputEnter = document.querySelector('.enterSng');
 
-const Display = () => {
-  Lists.forEach((e) => {
-    const li = document.createElement('li');
-    li.innerHTML = `<div class="inputDescripation"> <input type="checkbox" /> <span class="descriptions" >${e.description}</span> </div> <div class="bin">  </div>`;
-    li.setAttribute('key', e.index);
-    if (e.completed) {
-      li.classList.add('li');
+let tasks = [];
+
+inputEnter.addEventListener('click', (() => {
+  if (input.value === '') {
+    alert('please type');
+  } else {
+    const newtask = {
+      description: input.value,
+      completed: false,
+      index: tasks.length,
+    };
+    tasks.push(newtask);
+    todoFullList.innerHTML = '';
+    tasks.forEach((e, index) => {
+      todoFullList.innerHTML += `
+          <div>
+            <li class="todo-child" id=${index}>
+              <div>
+              <input class="checkbox" type="checkbox" ${(e.completed) ? 'checked' : ''}>
+               <span> ${e.description} </span>
+              </div>
+              <div class=bin></div>
+            </li>
+          </div>
+        `;
+    });
+    input.value = '';
+    localStorage.setItem('todo-list', JSON.stringify(tasks));
+  }
+}));
+
+window.addEventListener('load', () => {
+  const getItem = JSON.parse(localStorage.getItem('todo-list'));
+  if (getItem) {
+    todoFullList.innerHTML = getItem.map((e, index) => `
+        <div>
+          <li class="todo-child" id=${index}>
+            <div>
+            <input class="checkbox" type="checkbox" ${(e.completed) ? 'checked' : ''}>
+            <span> ${e.description} </span>
+            </div>
+            <div class=bin></div>
+          </li>
+        </div>
+      `).join('');
+    tasks = getItem;
+
+    document.querySelectorAll('.checkbox').forEach((checkbox, index) => {
+      checkbox.addEventListener('click', function () {
+        tasks[index].completed = this.checked;
+        localStorage.setItem('todo-list', JSON.stringify(tasks));
+      });
+    });
+  }
+});
+
+todoFullList.addEventListener('click', (e) => {
+  if (e.target.classList[0] === 'bin') {
+    e.target.parentElement.remove();
+    const newArray = tasks
+      .filter((element, index) => index !== parseInt(e.target.parentElement.id, 10))
+      .map((element, index) => {
+        element.index = index;
+        return element;
+      });
+    const li = document.querySelectorAll('.todo-child');
+    for (let i = 0; i < newArray.length; i += 1) {
+      newArray[i].index = i;
+      li[i].id = i;
     }
-    todoFullList.appendChild(li);
-  });
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-  Display();
+    tasks = newArray;
+    localStorage.setItem('todo-list', JSON.stringify(newArray));
+  }
 });
